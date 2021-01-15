1f37f4eb71c81f928d64a913891c10514b4bdd84
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-01-15 11:46:33 -0700

    Create reader.bat

---
+++ reader.bat
@@ -0,0 +1,5 @@
+::ReadScript
+::Sorry It Is Simple RN I am just testing basics
+find "saybyeworld{}" %file% && echo Bye World 
+find "sayhiworld{}" %file% && echo Hi World!
+find "promptforpause{}" %file% && pause
